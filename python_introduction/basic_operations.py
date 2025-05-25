e9cc8b8da4b231ca5833b2bf25673a5f757b2de9
Jembi Mary <Jembimary11@gmail.com>
2025-05-25 19:03:55 +0200

    Create basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+addition_message = "Addition of " + str(number1) + " and " + str(number2) + " is " + str(sum)
+subtraction_message = "Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(difference)
+product_message = "Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(product)
+print(addition_message)
+print(subtraction_message)
+print(product_message)
+

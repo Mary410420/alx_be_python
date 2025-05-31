2ef3575a1924c5bae07bbc3bb72cfcddd3cd607c
Jembi Mary <Jembimary11@gmail.com>
2025-05-31 21:13:54 +0200

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
 nunmber = int(input("Enter a number to see its multiplication table:"))
 
 for num in range(1, 11):
-    print(str(nunmber), "*", num, "=", str(nunmber * num))
+    print(str(nunmber) + " * " + num + " = " + str(nunmber * num))

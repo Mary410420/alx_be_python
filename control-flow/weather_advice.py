874347162e9a794234f96637e70dbf3270fb03f1
Jembi Mary <Jembimary11@gmail.com>
2025-05-31 20:27:11 +0200

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -3,11 +3,10 @@ advise = ""
 
 if weather == "sunny":
     advise = "Wear a t-shirt and sunglasses"
-elif weather == "rainy":
+if weather == "rainy":
     advise = "Don't forget your umbrella and a raincoat"
-elif weather == "cold":
+if weather == "cold":
     advise = "Make sure to wear a warm coat and a scarf"
-else:
-    advise = "Sorry, I don't have recommendations for this weather"
 
-print(advise)
+if advise == "":
+    print("Sorry, I don't have recommendations for this weather")

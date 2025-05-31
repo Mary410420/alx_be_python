9156c8f12bc29518275287d306f64691f02d913e
Jembi Mary <Jembimary11@gmail.com>
2025-05-31 21:02:35 +0200

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,18 +3,21 @@ num2 = input("Enter the second number:")
 
 type_of_operation = input("Choose the operation (+, -, *, /):")
 
-if type_of_operation == "+":
-    result = int(num1) + int(num2)
-    print("The result is " + str(result) + ".")
-elif type_of_operation == "-":
-    result = int(num1) - int(num2)
-    print("The result is " + str(result) + ".")
-elif type_of_operation == "*":
-    result = int(num1) * int(num2)
-    print("The result is " + str(result) + ".")
-elif type_of_operation == "/":
-    if num2 == int(0):
-        print("Cannot divide by zero.")
-    else:
-        result = int(num1) / int(num2)
+match type_of_operation:
+    case "+":
+        result = int(num1) + int(num2)
         print("The result is " + str(result) + ".")
+    case "-":
+        result = int(num1) - int(num2)
+        print("The result is " + str(result) + ".")
+    case "*":
+        result = int(num1) * int(num2)
+        print("The result is " + str(result) + ".")
+    case "/":
+        if num2 == int(0):
+            print("Cannot divide by zero.")
+        else:
+            result = int(num1) / int(num2)
+            print("The result is " + str(result) + ".")
+    case _:
+        print("Invalid day entered.")
